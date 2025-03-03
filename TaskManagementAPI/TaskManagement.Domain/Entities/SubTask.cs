bec216b491de4994b300d3fd466f8c3cac97fa3b
diegoNZ04 <diegoamorim03152004@gmail.com>
2025-03-03 15:43:51 -0300

    Arrumando Entidades

--- TaskManagementAPI/TaskManagement.Domain/Entities/SubTask.cs
+++ TaskManagementAPI/TaskManagement.Domain/Entities/SubTask.cs
@@ -5,25 +5,16 @@ namespace TaskManagement.Domain.Entities;
 
 public class SubTask
 {
-    // Id
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    // Description
-    [Required]
-    [MaxLength(255)]
     public string Description { get; set; } = string.Empty;
-    // FK UserTaskId
-    [Required]
     public int UserTaskId { get; set; }
-    // Navigation Property
+    [ForeignKey("UserTaskId")]
     public UserTask UserTask { get; set; } = null!;
     // IsCompleted
     public bool IsCompleted { get; set; } = false;
     // CompletedAt 
     [DataType(DataType.DateTime)]
     public DateTime? CompletedAt { get; set; } = null;
-    // DueDate
-    [DataType(DataType.DateTime)]
-    public DateTime? DueDate { get; set; } = null;
 }
--- TaskManagementAPI/TaskManagement.Domain/Entities/UserTask.cs
+++ TaskManagementAPI/TaskManagement.Domain/Entities/UserTask.cs
@@ -6,33 +6,21 @@ namespace TaskManagement.Domain.Entities;
 
 public class UserTask
 {
-    // Id
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    // Title
-    [Required]
-    [MaxLength(255)]
     public string Title { get; set; } = string.Empty;
-    // Description
-    [Required]
-    [MaxLength(255)]
     public string Description { get; set; } = string.Empty;
-    // Priority
-    [Required]
     public TaskPriority Priority { get; set; }
     public ICollection<SubTask>? SubTasks { get; set; } = [];
     // FK UserId
-    [Required]
     public int UserId { get; set; }
     // Navigation Property
+    [ForeignKey("UserId")]
     public User User { get; set; } = null!;
     // IsCompleted
     public bool IsCompleted { get; set; } = false;
     // CompletedAt 
     [DataType(DataType.DateTime)]
     public DateTime? CompletedAt { get; set; } = null;
-    // DueDate
-    [DataType(DataType.DateTime)]
-    public DateTime? DueDate { get; set; } = null;
 }
