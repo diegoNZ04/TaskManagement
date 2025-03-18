573b0b0262810d629d68975a4742e9e3d768b2dd
diegoNZ04 <diegoamorim03152004@gmail.com>
2025-03-18 11:38:51 -0300

    Arrumando DTOs de Request do Endpoint Put de UserTask

--- TaskManagementAPI/TaskManagement.Application/Dtos/Requests/UserTasksRequests/UpdateTaskRequest.cs
+++ TaskManagementAPI/TaskManagement.Application/Dtos/Requests/UserTasksRequests/UpdateTaskRequest.cs
@@ -1,7 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using TaskManagement.Domain.Enums;
+
 namespace TaskManagement.Application.Dtos.Requests.UserTasksRequests
 {
-    public class UpdateTaskRequest : CreateTaskRequest
+    public class UpdateTaskRequest
     {
-
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(255)]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public TaskPriority Priority { get; set; }
     }
 }
