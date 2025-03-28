b9edbd151c75ac97e28169298580f553525afee9
diegoNZ04 <diegoamorim03152004@gmail.com>
2025-03-28 16:29:30 -0300

    Preparações para desenvolvimento do front

--- TaskManagementApp/src/app/app.routes.ts
+++ TaskManagementApp/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ManagementPanelComponent } from './pages/management-panel/management-panel.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/management-panel', pathMatch: 'full' },
+  { path: 'management-panel', component: ManagementPanelComponent },
+];
---
+++ TaskManagementApp/src/app/pages/management-panel/management-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-management-panel',
+  imports: [],
+  templateUrl: './management-panel.component.html',
+  styleUrl: './management-panel.component.css'
+})
+export class ManagementPanelComponent {
+
+}
